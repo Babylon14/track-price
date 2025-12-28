612a09838328de23c71abfba05873e46107573cb
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2025-12-28 15:36:56 +0300

    Сборка Docker контейнера для приложения на FastAPI, PostgreSql БД, PgAdmin и redis.

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
 """ГЛОБАЛЬНАЯ ТОЧКА ВХОДА"""
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Price Tracker API v1",
+    description="API price tracker from marketplace v1",
+    terms_of_service="",
+)
+
+@app.get("/")
+async def root():
+    return {"message": "hello world!"}
 
 
--- app/server.py
+++ app/server.py
@@ -1,13 +1,3 @@
 """Сборщик всех роутеров v1"""
-from fastapi import FastAPI
 
-app = FastAPI(
-    title="API v1",
-    description="API price tracker from marketplace v1",
-    terms_of_service="",
-)
-
-@app.get("/")
-async def cmd_hello():
-    return {"hello": "hello world!"}
 
