5ff9a2da7e841c3db9cc705b94602868fd4cf6e2
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2025-12-28 16:01:17 +0300

    Добавлена модель История цен

--- app/models/product.py
+++ app/models/product.py
@@ -25,4 +25,14 @@ class Product(Base):
 
 
 class PriceHistory(Base):
-    pass
+    __tablename__ = "price_history"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    product_id: Mapped[float] = mapped_column(ForeignKey("products.id"), onupdate="CASCADE")
+    price: Mapped[float] = mapped_column(Float)
+    timestamp: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+
+    # Обратная связь
+    product: Mapped["Product"] = relationship(back_populates="prices")
+
+    
